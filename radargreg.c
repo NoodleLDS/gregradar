1f246ec3ffcb070b405bbd8a84b624c41971e5e8
NoodleLDS <noodle_oliveira@hotmail.com>
2025-01-27 17:24:29 -0300

    gregradar

---
+++ radargreg.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int idade;
+
+    printf("Vamos ver se o Greg gosta de voce...\n");
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+
+    if (idade >= 15) {
+        printf("Voce nao e para o Greg.\n");
+    } else {
+        printf("Voce e para Gameles.\n");
+    }
+    return 0;
+}
